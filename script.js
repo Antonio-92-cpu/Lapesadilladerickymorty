46e571ae3aebf1e2c83d9e5374690225ba46444d
Chachi20 <santanahormigaencarna@gmail.com>
2023-06-23 11:45:01 +0100

    complicaciones con el pop up

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function openDialog() {
+  $('#overlay').fadeIn('fast', function() {
+      $('#popup').css('display','block');
+      $('#popup').animate({'left':'30%'},500);
+  });
+}
+
+function closeDialog(id) {
+  $('#'+id).css('position','absolute');
+  $('#'+id).animate({'left':'-100%'}, 500, function() {
+      $('#'+id).css('position','fixed');
+      $('#'+id).css('left','100%');
+      $('#overlay').fadeOut('fast');
+  });
+}
+
 const searchInput = document.getElementById('search-input');
 const searchButton = document.getElementById('search-button');
 const characterList = document.getElementById('character-list');
@@ -58,5 +74,6 @@ function fetchCharacters() {
 fetchCharacters();
 
 document.getElementById("reset-button").addEventListener('click', fetchCharacters); 
+
 
 
